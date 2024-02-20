721b0297ff5789213e7cd640ca4f9fe723e70b47
claude0237 <claudenkoma@gmail.com>
2024-02-20 18:15:11 +0000

    deploiement docker sur serveur distant

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-application = "pythonapp"
-dockerhubaccountid = "claudenkoma"
-
-	echo "Deploy App!"
-        docker run -d -p 3333:3333 ${dockerhubaccountid}/${application}:${BUILD_NUMBER}
-	echo "Remove old images"
-	docker rmi ${dockerhubaccountid}/${application}:latest -f
